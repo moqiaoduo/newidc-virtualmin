c2d0d10fb295df77401d9b5bce26cc45d89c01de
moqiaoduo <415372363@qq.com>
2022-06-25 01:01:18 +0800

    修复参数

--- src/Plugin.php
+++ src/Plugin.php
@@ -58,7 +58,7 @@ class Plugin extends Server
     {
         $result = $this->exec('disable-domain', [
             'why' => $this->service->extra['suspend_reason'],
-            'domain' => $this->server->domain
+            'domain' => $this->service->domain
         ]);
         if (!isset($result['status']) || $result['status'] != 'success') {
             return ['code' => 1, 'msg' => $result['error'] ?? '未知错误'];
@@ -73,7 +73,7 @@ class Plugin extends Server
     public function unsuspend()
     {
         $result = $this->exec('enable-domain', [
-            'domain' => $this->server->domain
+            'domain' => $this->service->domain
         ]);
         if (!isset($result['status']) || $result['status'] != 'success') {
             return ['code' => 1, 'msg' => $result['error'] ?? '未知错误'];
